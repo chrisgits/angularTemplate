bd01b0235db13df3f92614693ec85759a0e67a46
chrisgits <cmannel77@gmail.com>
2015-12-09 17:04:17 -0600

    Update flexbox and @media css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('todoListApp', [])
   .controller('mainCtrl', function ($scope) { /*method chaining starts this line*/
     $scope.helloWorld = function () {
-      console.log("Hello there, this is controller func in mainCtrl");
+      //do something;
     };
    }); /* creates app */
 
